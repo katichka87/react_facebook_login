a05522e851278a19e68ec8e11c702665b30a52cd
Ekaterina Ivanova <katichka87@gmail.com>
2015-12-15 22:46:59 +0200

    First commit

---
+++ android/app/src/main/java/com/sample/MainActivity.java
@@ -0,0 +1,118 @@
+package com.sample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.KeyEvent;
+
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.react.LifecycleState;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
+
+    private ReactInstanceManager mReactInstanceManager;
+    private ReactRootView mReactRootView;
+    private FacebookLoginPackage mFacebookLoginPackage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mFacebookLoginPackage = new FacebookLoginPackage(this);
+
+        mReactRootView = new ReactRootView(this);
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModuleName("index.android")
+                .addPackage(new MainReactPackage())
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .addPackage(mFacebookLoginPackage)
+                .build();
+
+        mReactRootView.startReactApplication(mReactInstanceManager, "sample", null);
+
+        setContentView(mReactRootView);
+
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.sample",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mFacebookLoginPackage.handleActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        //if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+            mReactInstanceManager.showDevOptionsDialog();
+            return true;
+        //}
+        //return super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    public void onBackPressed() {
+      if (mReactInstanceManager != null) {
+        mReactInstanceManager.onBackPressed();
+      } else {
+        super.onBackPressed();
+      }
+    }
+
+    @Override
+    public void invokeDefaultOnBackPressed() {
+      super.onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        AppEventsLogger.deactivateApp(this);
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AppEventsLogger.activateApp(this);
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onResume(this, this);
+        }
+    }
+}
---
+++ index.android.js
@@ -0,0 +1,111 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  Image,
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+  DrawerLayoutAndroid,
+  ProgressBarAndroid,
+  TouchableHighlight,
+} = React;
+
+
+var FBLoginManager = require('NativeModules').FBLoginManager;
+
+
+var sample = React.createClass({
+    getInitialState() {
+		var statics = {
+            loginText: 'Login with Facebook',
+            logoutText: 'Logout from Facebook'
+        };
+        return {
+            statics:statics,
+            isLoggedIn: false,
+            buttonText: statics.loginText
+        };
+    },
+
+	componentWillMount: function() {
+        var self = this;
+        FBLoginManager.getCurrentToken(function(token) {
+            if (typeof(token) === 'string' && token.length > 0) {
+                self.setState({isLoggedIn:true, buttonText: self.state.statics.logoutText});
+            } else {
+                self.setState({isLoggedIn:false, buttonText: self.state.statics.loginText});
+            }
+        })
+    },
+
+    _handleEvent(e, data) {
+        var result = e || data;
+        if(result.type === 'success' && result.profile) {
+            try{
+                result.profile = JSON.parse(result.profile)
+            } catch(err) {
+                console.warn('Could not parse facebook profile: ', result.profile);
+                console.error(err);
+            }
+        }
+        
+        if(result.eventName === 'onLogin' || result.eventName === 'onLoginFound') {
+            this.setState({isLoggedIn:true, buttonText: this.state.statics.logoutText});
+        } else if(result.eventName === 'onLogout') {
+            this.setState({isLoggedIn:false, buttonText: this.state.statics.loginText});
+        }
+        
+        if (result.eventName && this.props.hasOwnProperty(result.eventName)) {
+            var event = result.eventName;
+            delete result.eventName;
+            console.log('Triggering \'%s\' event', event)
+            this.props[event](result);
+        } else {
+            console.log('\'%s\' Event is not defined or recognized', result.eventName)
+        }
+    },
+	
+	_onFacebookPress() {
+        var permissions = ['email', 'public_profile'];
+        if (typeof(this.props.permissions) === 'array'){
+            permissions = this.props.permissions;
+        }
+        
+        if(this.state.isLoggedIn){
+            FBLoginManager.logout((err,data) => this._handleEvent(err,data));
+        } else {
+            FBLoginManager.loginWithPermissions(permissions, (err,data) => this._handleEvent(err,data));
+        }
+    },
+	
+    render: function() {
+        return (
+            <TouchableHighlight onPress={this._onFacebookPress}>
+                <View style={styles.login}>
+                    <Text style={styles.whiteFont}> {this.state.buttonText} </Text>
+                </View>
+            </TouchableHighlight>
+        )
+    }
+})
+
+var styles = StyleSheet.create({
+    login: {
+        flex: 1,
+        backgroundColor: '#3B5998',
+        padding: 10,
+        alignItems: 'center'
+    },
+    whiteFont: {
+        color: 'white'
+    }
+});
+
+AppRegistry.registerComponent('sample', () => sample);
